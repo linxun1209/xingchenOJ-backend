421aa2b6ceb0dc88418be6b3acc36a78575aa87b
linxun1209 <3056726345@qq.com>
2023-08-08 22:29:18 +0800

    第二期：关于题目以及提交题目的相关接口开发

---
+++ src/main/java/com/xingchen/oj/controller/QuestionSubmitController.java
@@ -0,0 +1,80 @@
+package com.xingchen.oj.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xingchen.oj.common.BaseResponse;
+import com.xingchen.oj.common.ErrorCode;
+import com.xingchen.oj.common.ResultUtils;
+import com.xingchen.oj.exception.BusinessException;
+import com.xingchen.oj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.xingchen.oj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
+import com.xingchen.oj.model.entity.QuestionSubmit;
+import com.xingchen.oj.model.entity.User;
+import com.xingchen.oj.model.vo.QuestionSubmitVO;
+import com.xingchen.oj.service.QuestionSubmitService;
+import com.xingchen.oj.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 做题记录接口
+ *
+ * @author xingchen
+ * @from xingchen
+ */
+@RestController
+@RequestMapping("/question_ubmit")
+@Slf4j
+public class QuestionSubmitController {
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 提交题目
+     *
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return 提交记录的 id
+     */
+    @PostMapping("/")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+                                               HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+    }
+
+    /**
+     * 分页获取题目提交列表（除了管理员外，普通用户只能看到非答案、提交代码等公开信息）
+     *
+     * @param questionSubmitQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest,
+                                                                         HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        // 从数据库中查询原始的题目提交分页信息
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        final User loginUser = userService.getLoginUser(request);
+        // 返回脱敏信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
+    }
+
+}
