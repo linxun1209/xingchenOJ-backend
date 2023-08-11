0e31858c3b6d0d61e7d174a21ca0c2d1bfea80fa
linxun1209 <3056726345@qq.com>
2023-08-11 20:25:40 +0800

    第三期：完善用户根据id获得自己发布的题目数据接口

--- src/main/java/com/xingchen/oj/controller/QuestionController.java
+++ src/main/java/com/xingchen/oj/controller/QuestionController.java
@@ -142,6 +142,22 @@ public class QuestionController {
         return ResultUtils.success(result);
     }
 
+    @GetMapping("/get")
+    public BaseResponse<Question> getQuestionById(long id, HttpServletRequest request) {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = questionService.getById(id);
+        if (question == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        User loginUser=userService.getLoginUser(request);
+        if(!question.getUserId().equals(loginUser.getId())&&!userService.isAdmin(loginUser)){
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        return ResultUtils.success(question);
+    }
+
     /**
      * 根据 id 获取
      *
